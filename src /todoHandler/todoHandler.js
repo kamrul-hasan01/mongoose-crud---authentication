cf51f6eba6c88ac0d028e9360aa5dbd3517e46af
kamrul-hasan01 <k17h02@gmail.com>
2022-06-15 17:26:31 +0600

    get and putt are not  done

--- src /todoHandler/todoHandler.js
+++ src /todoHandler/todoHandler.js
@@ -5,8 +5,19 @@ const todoSchema = require("../schemas/todoSchema");
 const Todo = new mongoose.model("Todo", todoSchema);
 // get all todo
 router.get("/", async (req, res) => {
-  console.log("test");
-  res.send("get");
+  await Todo.find({}, (error, data) => {
+    console.log(error, data);
+    if (error) {
+      res.status(500).json({
+        error,
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: "Todo load successfully",
+      });
+    }
+  });
 });
 // get   todo by id
 router.get("/:id", async (req, res) => {});
@@ -21,15 +32,48 @@ router.post("/", async (req, res) => {
       });
     } else {
       res.status(200).json({
-        message: "Todo was inserted successfully",
+        message: "Todo inserted successfully",
       });
     }
   });
 });
 // post multiple  todo
-router.post("/all ", async (req, res) => {});
+router.post("/all", async (req, res) => {
+  await Todo.insertMany(req.body, (error) => {
+    if (error) {
+      res.status(500).json({
+        error,
+      });
+    } else {
+      res.status(200).json({
+        message: "Todos inserted successfully",
+      });
+    }
+  });
+});
 //  update todo
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  console.log(req.params.id);
+  await Todo.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        title: "active",
+      },
+    },
+    (error) => {
+      if (error) {
+        res.status(500).json({
+          error,
+        });
+      } else {
+        res.status(200).json({
+          message: "Todo updated successfully",
+        });
+      }
+    }
+  );
+});
 //  delete todo
 router.delete("/:id", async (req, res) => {});
 
